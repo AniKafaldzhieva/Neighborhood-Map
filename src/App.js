ec0aac13a4a6fe7704212b132a95a392aaa712f3
AniKafaldzhieva <anikafaldzhieva@abv.bg>
2018-08-28 11:06:36 +0300

    Add alert message

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
 import React, { Component } from 'react';
 import { GoogleApiWrapper } from 'google-maps-react' 
 import MapContainer from './MapContainer';
+import ErrorBoundary from './ErrorBoundary'
 import './App.css';
+
+window.gm_authFailure = () => {
+  alert("Something went wrong. Please, check your Google API key!")
+}
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <ErrorBoundary>
         <MapContainer 
           google={this.props.google}
         />
+      </ErrorBoundary>
       </div>
     );
   }
